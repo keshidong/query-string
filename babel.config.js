530e333900ef341c79511336edb5d18b5c8f78de
shidong.ke <shidong.ke@shopee.com>
2018-12-02 21:49:41 +0800

    minify code

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,15 @@
 
 module.exports = {
     presets: [
+        ['minify', {
+            'deadcode': true,
+            'removeConsole': true,
+            'removeDebugger': true,
+            'simplify': true,
+        }],
         ['@babel/preset-env', {
             targets: '> 0.2%, not dead',
         }]
     ],
+    'comments': false,
 };
